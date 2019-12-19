6a79fb0a83d3261c994c592ee7959bdb6861d60c
pornpol <pornpol.w@gmail.com>
2019-12-19 23:26:04 +0700

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage/HomePage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
