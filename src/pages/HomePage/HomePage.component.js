adc553822c3f743396976a1762c7ac40d6055828
pornpol <pornpol.w@gmail.com>
2019-12-19 21:35:56 +0700

    Create component & stlyling MenuItem

---
+++ src/pages/HomePage/HomePage.component.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.style.scss";
+
+import Directory from "../../components/Directory/Directory.component";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
