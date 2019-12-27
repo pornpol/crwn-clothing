a51f50e31f311c0090f0ecdfd091f52d269c6bfe
pornpol <pornpol.w@gmail.com>
2019-12-27 10:58:07 +0700

    Intregrate Stripe api payment gateway

--- src/pages/CheckoutPage/CheckoutPage.component.js
+++ src/pages/CheckoutPage/CheckoutPage.component.js
@@ -9,6 +9,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from "../../components/CheckoutItem/CheckoutItem.component";
+import StripeCheckoutButton from "../../components/StripeCheckoutButton/StripeCheckoutButton.component";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -33,6 +34,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
     <div className="total">TOTAL:${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for payment*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
